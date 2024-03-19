5601c63ee37c038bc7012b5d298038f13367a61a
Sophie Dickerson (1000115856) <DICKSS3@student.op.ac.nz>
2024-03-19 15:57:28 +1300

    got a working nav bar adding in more pages

--- src/lib/conponents/Header.svelte
+++ src/lib/conponents/Header.svelte
@@ -1,16 +1,13 @@
-<div class="header">
+<div class="body">
     <!-- Logo img here -->
 
     <h1>Find My Flatmate</h1>
+    <nav>
+        <ul>
+            <li><a href="/ourstory">Our Story</a></li>
+            <li><a href="/shop">Shop</a></li>
+            <li><a href="/journey">The Journey</a></li>
+        </ul>  
+    </nav>
     
-    <ul>
-        <li><a href="/About">Our Story</a></li>
-        <li><a href="/About">Shop</a></li>
-        <li><a href="/About">The Journey</a></li>
-    </ul>
 </div>
-
-
-<style>
-   
-</style>
