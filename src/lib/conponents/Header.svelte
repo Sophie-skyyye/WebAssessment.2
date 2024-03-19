c701ecdbabee5455d5395bd4e50c8f2a1752d46a
Sophie Dickerson (1000115856) <DICKSS3@student.op.ac.nz>
2024-03-19 15:10:13 +1300

    added titel and page links

---
+++ src/lib/conponents/Header.svelte
@@ -0,0 +1,11 @@
+<div class="header">
+    <!-- Logo img here -->
+
+    <h1>Find My Flatmate</h1>
+    
+    <ul>
+        <li><a href="/About">Our Story</a></li>
+        <li><a href="/About">Shop</a></li>
+        <li><a href="/About">The Journey</a></li>
+    </ul>
+</div>
