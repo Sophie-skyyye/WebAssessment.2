ca3652c3936d701a38d2fa53fc481fd917e73fd1
Sophie-skyyye <sophieskyed@gmail.com>
2024-03-26 10:33:41 +1300

    added to the ourstory page

---
+++ src/routes/ourstory/+page.svelte
@@ -0,0 +1,12 @@
+<p>
+    Our journey began with a moment of concern and practicality. As the designated sober driver
+    for the night, I faced an unsettling situation when my flatmate's phone turned off during a night out. As a flat 
+    we relied on apps like Life360 for reassurance when someones on a night out.
+    I didn't get the usual 1 am call to pick up my flatmate. I would send her a text 
+    letting her know I'm still ok to pick her up. By 2:30 am, her phone was off, rendering conventional tracking ineffective.
+    This experience made me realize the limitations of existing tracking solutions, particularly 
+    in dynamic environments like nightlife. It sparked the idea for Find My Flatmate—a device 
+    designed to provide reliable location updates independent of smartphones
+    Find My Flatmate isn't just a product; it's a practical solution born from real-world challenges. 
+    It addresses the need for reliable safety measures in social settings, offering peace of mind to users.
+</p>
